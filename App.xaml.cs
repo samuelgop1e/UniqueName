00aff139e1e91bc39adebb85f866bcbfc3273822
samuelgop1e <samuel.gopie@mail.utoronto.ca>
2024-02-02 15:22:35 -0500

    Initial Commit

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace UniqueName
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace UniqueName
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
